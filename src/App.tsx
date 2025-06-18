5ea5100f05f8f6bb194a0445567c39fd63684660
Mugamba669 <brunohectre@gmail.com>
2025-06-18 09:49:39 +0300

    modified cookies

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,12 @@ import Register from "./pages/Register";
 import ForgotPasswordPage from "./pages/ForgotPassword";
 import WaitScreen from "./pages/WaitScreen";
 import { ClientProvider } from "./hooks/ClientContext";
+import { isAuthenticated } from "./lib/cookies/UserMangementCookie";
 // import { Toaster } from "./components/ui/sonner";
 
 function App() {
+
+  const auth = isAuthenticated();
   return (
     <>
       <Toaster />
@@ -37,31 +40,40 @@ function App() {
         <ClientProvider>
           <BrowserRouter>
             <Routes>
-              <Route element={<PrivateRoutes />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/beneficiaries" element={<BeneficiariesPage />} />
-                <Route path="/transactions" element={<Transactions />} />
-                <Route path="/staff" element={<Staff />} />
-                <Route path="/audit-logs" element={<AuditLogs />} />
-                <Route path="/applications" element={<ApplicationsPage />} />
-                <Route path="/organisations" element={<Organisations />} />
-                <Route path="/settings" element={<SettingsPage />} />
-                {/* //Admin Routes */}
-                <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                <Route
-                  path="/admin/transactions"
-                  element={<AdminTransactions />}
-                />
-                <Route path="/admin/manuals" element={<ManualTransactions />} />
-              </Route>
+              {
+                auth ? (<>
+                  {/* auth routes */}
+                  <Route element={<PrivateRoutes />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/beneficiaries" element={<BeneficiariesPage />} />
+                    <Route path="/transactions" element={<Transactions />} />
+                    <Route path="/staff" element={<Staff />} />
+                    <Route path="/audit-logs" element={<AuditLogs />} />
+                    <Route path="/applications" element={<ApplicationsPage />} />
+                    <Route path="/organisations" element={<Organisations />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                    {/* //Admin Routes */}
+                    <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                    <Route
+                      path="/admin/transactions"
+                      element={<AdminTransactions />}
+                    />
+                    <Route path="/admin/manuals" element={<ManualTransactions />} />
+                  </Route>
+                  {/* end of auth routes */}
+                </>) : (<>
 
-              <Route path="/" element={<LandingPage />} />
+                  {/* non auth routes */}
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/wait-approval" element={<WaitScreen />} />
+                  <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/about" element={<AboutUs />} />
+                  {/* end of non auth routes */}</>)
+              }
+
               <Route path="*" element={<PageNotFound />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/wait-approval" element={<WaitScreen />} />
-              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/about" element={<AboutUs />} />
             </Routes>
           </BrowserRouter>
         </ClientProvider>
