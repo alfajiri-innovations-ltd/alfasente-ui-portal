1e3b23c35aa18730ffaaaba38ae4af636033865b
¨Nanyonga-Rahmah¨ <nanyongarahmah891@gmail.com>
2025-08-22 10:06:49 +0300

    Invited User

---
+++ src/pages/SetPassword.tsx
@@ -0,0 +1,13 @@
+import { ResetPasswordForm } from "@/components/AuthForms/ResetPassword"
+
+function SetPassword() {
+  return (
+    <div>
+
+        <ResetPasswordForm/>
+      
+    </div>
+  )
+}
+
+export default SetPassword
