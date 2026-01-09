2c370b6f1234fbcf162a4feabb9f295f4cdee824
¨Nanyonga-Rahmah¨ <nanyongarahmah891@gmail.com>
2026-01-09 21:37:06 +0300

    Send funds

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2m6tgnabp2"
+    "revision": "0.6u4vdt103kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
